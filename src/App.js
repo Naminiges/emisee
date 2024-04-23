5d173d2385b18ece60b582764e0db4237c4d1b8a
KenwardKH <kenwardkh5@gmail.com>
2024-04-23 09:29:13 +0700

    Menghubungkan ke react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import './App.css';
-import { Auth } from './components/auth';
-import { SignUp } from './components/signUp';
-import { Home } from './pages/home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Auth } from "./components/auth";
+import { SignUp } from "./components/signUp";
+import { Home } from "./pages/home";
+import { Index } from "./components/index";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<SignUp />}></Route>
-          <Route path='/login' element={<Auth />}></Route>
-          <Route path='/home' element={<Home />}></Route>
+          <Route path="/" element={<Index />}></Route>
+          <Route path="/signUp" element={<SignUp />}></Route>
+          <Route path="/login" element={<Auth />}></Route>
+          <Route path="/home" element={<Home />}></Route>
         </Routes>
       </Router>
-
     </div>
   );
 }
--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,85 +1,95 @@
 import { useState } from "react";
 import { auth, googleProvider } from "../config/firebase";
-import { createUserWithEmailAndPassword, sendEmailVerification, signInWithPopup } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import googleLogo from "../image/7123025_logo_google_g_icon.svg"
 
 export const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
-
   const signUp = async (e) => {
     e.preventDefault();
     try {
-      await createUserWithEmailAndPassword(auth,email,password);
-      await sendEmailVerification(auth.currentUser);
-      alert("Email have been sent")
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("Account created succesfully");
+      navigate("/home")
     } catch (err) {
-      if(err.code === "auth/email-already-in-use"){
+      if (err.code === "auth/email-already-in-use") {
         alert("Email already in use");
       } else {
-        alert(err.message);
+        alert("Email invalid");
       }
       console.error(err);
     }
   };
 
-  const signInWithGoogle = async() => {
-    try{
-        await signInWithPopup(auth, googleProvider)
-        navigate("/home")
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+      navigate("/home");
     } catch (err) {
-        console.error(err);
+      console.error(err);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={signUp}>
-        <table>
-          <tr>
-            <td>Masukkan Nama Email</td>
-            <td> : </td>
-            <td>
-              <input
-                placeholder="Email Username"
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>Masukkan Password</td>
-            <td> : </td>
-            <td>
-              <input
+    <html>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Login Page</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <link rel="stylesheet" href="signup.css" />
+      </head>
+      <body>
+        <div class="container">
+          <div class="header"></div>
+          <div class="content">
+            <p>Sign up For Free</p>
+          </div>
+
+          <div class="form-container">
+            <p>Email</p>
+            <input
+              type="text"
+              placeholder="Name"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <p>Password</p>
+            <input
                 placeholder="Password"
                 type="password"
                 required
                 onChange={(e) => setPassword(e.target.value)}
               />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button type="submit">Sign Up</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Already have account?</td>
-            <td>
-              <button onClick={() => navigate("/login")}>Login</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Sign in using google</td>
-            <td>
-            <button onClick={signInWithGoogle}>Sign In With Google</button>
-            </td>
-          </tr>
-        </table>
-      </form>
-    </div>
+            <button class="signup" type="submit" onClick={signUp}>
+              Sign up
+            </button>
+            <p style={{textAlign: "center"}}>or</p>
+            <button class="continuewith" onClick={signInWithGoogle}>
+              <img src={googleLogo} alt="Google Logo" />
+              Continue with Google
+            </button>
+
+            <div class="termofuse">
+              <p>
+                By creating an account, you agree to our
+                <span class="highlighted">Terms of Use</span> and
+                <span class="highlighted">Privacy Policy</span>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 };
