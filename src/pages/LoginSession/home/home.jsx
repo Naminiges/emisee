d14bb756991a6067f43c2221296c07c47e1396aa
KenwardKH <kenwardkh5@gmail.com>
2024-04-30 16:31:11 +0700

    home habis login

--- src/pages/LoginSession/home/home.jsx
+++ src/pages/LoginSession/home/home.jsx
@@ -1,13 +1,250 @@
-import React from 'react'
+import React from "react";
 import Navbarlogin from "../../../components/navbarLogin/Navbarlogin";
 import Footer from "../../../components/footer/footer";
+import './home.css';
 
 export const Home = () => {
   return (
     <div>
-        <Navbarlogin />
-        home
-        <Footer />
+      <Navbarlogin />
+      <div className="container">
+        <div className="top">
+          <div>
+            <h1>Temukan Siapa Yang Ingin Kamu Bantu</h1>
+          </div>
+        </div>
+        <div className="main">
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="img"></div>
+            <div className="card-text">
+              <div className="judul">Perjuangan Anak Petani Melawan Penyakit Lumpuh Otak</div>
+              <div className="text">Orang tua mana yang tidak khawatir kalau anaknya tidak menunjukkan perkembangan yang baik. Itulah yang...</div>
+              <div className="dana">
+                <div className="dana-top">
+                  <div>Dana terkumpul</div>
+                  <div>Rp 2.590.000</div>
+                </div>
+                <div className="line">
+                  <div className="kumpul"></div>
+                  <div className="sisa"></div>
+                </div>
+                <div className="bottom">Dari Rp 8.840.000</div>
+              </div>
+              <button className="card-btn">Donasi</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
