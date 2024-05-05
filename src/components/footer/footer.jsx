9cc3cbd925875b2595d7494b7fc130a00a2fc57d
Putera Nami Shiddieqy <105465764+Naminiges@users.noreply.github.com>
2024-05-05 19:26:20 +0700

    feat(footer): logic islogedin

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom"; // Tambahkan useLocation
 import "./Footer.css";
 import logo from "../../image/logo.jpg";
 
 const Footer = () => {
+  const navigate = useNavigate();
+  const location = useLocation(); // Gunakan useLocation untuk mendapatkan lokasi halaman saat ini
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const currentUser = localStorage.getItem("currentUser");
+    if (currentUser) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
   return (
     <footer className="footer">
       <div className="footer-column">
@@ -12,11 +25,25 @@ const Footer = () => {
       </div>
       <div className="footer-column">
         <h3>Tunjukkan Aksimu</h3>
-        <ul>
-          <li>Donasi</li>
-          <li>Menggalang Dana</li>
-          <li>Tanam Kebaikan</li>
-        </ul>
+        {isLoggedIn ? (
+          <ul>
+            <li>
+              <a href="/home">Donasi</a>
+            </li>
+            <li>
+              <a href="//galang-dana">Menggalang Dana</a>
+            </li>
+          </ul>
+        ) : (
+          <ul>
+            <li>
+              <a href="/loginregister">Donasi</a>
+            </li>
+            <li>
+              <a href="/loginregister">Menggalang Dana</a>
+            </li>
+          </ul>
+        )}
       </div>
       <div className="footer-column">
         <h3>Pelajari Lebih Lanjut</h3>
@@ -36,8 +63,12 @@ const Footer = () => {
       <div className="footer-column">
         <h3>Terhubung</h3>
         <ul>
-          <li><a href="/faq">FAQ</a></li>
-          <li><a href="/kontak">Kontak Kami</a></li>
+          <li>
+            <a href="/faq">FAQ</a>
+          </li>
+          <li>
+            <a href="/kontak">Kontak Kami</a>
+          </li>
         </ul>
       </div>
     </footer>
